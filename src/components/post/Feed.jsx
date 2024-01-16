04df2a3ee8c9defa94507db6d4b71bc0b9b6b11d
vimukthimadushan94 <madushan.gangoda94@gmail.com>
2024-01-16 23:10:10 +0200

    fix ui for feed

--- src/components/post/Feed.jsx
+++ src/components/post/Feed.jsx
@@ -1,3 +1,67 @@
+import dummyImage from "../../assets/images/products/s4.jpg";
+import RightBar from "../site/RightBar";
+import { useSelector } from "react-redux";
+
 export default function Feed() {
-  return <h2>Feed Section</h2>;
+  const { avatarUrl } = useSelector((state) => state.auth);
+
+  return (
+    <>
+      <div class="container-fluid">
+        <div class="card">
+          <div class="card-body" style={{ width: "65%" }}>
+            <div class="col-md-12">
+              <h5 class="card-title fw-semibold mb-4">Feed</h5>
+              <div class="card">
+                <div class="card-body">
+                  <h5 class="card-title">
+                    <img
+                      src={avatarUrl}
+                      alt=""
+                      width="45"
+                      height="45"
+                      className="rounded-circle"
+                      style={{ "margin-right": "3%" }}
+                    />
+                    Madushan Gangoda
+                  </h5>
+                  <small>about an hour ago</small>
+                  <p class="card-text">
+                    Some quick example text to build on the card title and make
+                    up the bulk of the card's content.
+                  </p>
+                </div>
+                <div className="row">
+                  <div className="col-md-6">
+                    <img src={dummyImage} class="card-img-top" alt="..." />
+                  </div>
+                  <div className="col-md-6">
+                    <img src={dummyImage} class="card-img-top" alt="..." />
+                  </div>
+                </div>
+              </div>
+              <div class="card">
+                <div class="card-body">
+                  <h5 class="card-title">Card title</h5>
+                  <p class="card-text">
+                    Some quick example text to build on the card title and make
+                    up the bulk of the card's content.
+                  </p>
+                </div>
+                <div className="row">
+                  <div className="col-md-6">
+                    <img src={dummyImage} class="card-img-top" alt="..." />
+                  </div>
+                  <div className="col-md-6">
+                    <img src={dummyImage} class="card-img-top" alt="..." />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <RightBar />
+    </>
+  );
 }
---
+++ src/components/site/RightBar.jsx
@@ -0,0 +1,62 @@
+import { Link } from "react-router-dom";
+import logo from "../../assets/images/logos/logo.png";
+
+export default function RightBar() {
+  return (
+    <aside className="right-sidebar">
+      <div>
+        <div className="brand-logo d-flex align-items-center justify-content-between">
+          <Link href="./index.html" className="text-nowrap logo-img">
+            <img src={logo} width="180" alt="" style={{ width: "45%" }} />
+          </Link>
+          <div
+            className="close-btn d-xl-none d-block sidebartoggler cursor-pointer"
+            id="sidebarCollapse"
+          >
+            <i className="ti ti-x fs-8"></i>
+          </div>
+        </div>
+        <nav className="sidebar-nav scroll-sidebar" data-simplebar="">
+          <ul id="sidebarnav">
+            <li className="nav-small-cap">
+              <i className="ti ti-dots nav-small-cap-icon fs-4"></i>
+              <span className="hide-menu">Home</span>
+            </li>
+            <li className="sidebar-item">
+              <Link
+                to={"/profile"}
+                className="sidebar-link"
+                aria-expanded="false"
+              >
+                <span>
+                  <i className="ti ti-layout-dashboard"></i>
+                </span>
+                <span className="hide-menu">Profile</span>
+              </Link>
+            </li>
+            <li className="sidebar-item">
+              <Link
+                to={"/post/create"}
+                className="sidebar-link"
+                aria-expanded="false"
+              >
+                <span>
+                  <i className="ti ti-layout-dashboard"></i>
+                </span>
+                <span className="hide-menu">Create Post</span>
+              </Link>
+            </li>
+            <li className="sidebar-item">
+              <Link to={"/feed"} className="sidebar-link" aria-expanded="false">
+                <span>
+                  <i className="ti ti-layout-dashboard"></i>
+                </span>
+                <span className="hide-menu">Feed</span>
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </aside>
+  );
+}
